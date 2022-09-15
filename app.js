5f723fd9b4747935735e2488a1dd4dec27af3b70
MIRZA AHMER BAIG <mahmerbaig3@gmail.com>
2022-09-15 20:58:33 +0500

    half done
    half

---
+++ app.js
@@ -0,0 +1,10 @@
+// searchbar worked start //
+function searchbarfunction(){
+searchvalue=document.getElementById('search_bar_content').value;
+console.log(searchvalue)
+return   searchvalue
+}
+    
+ var a= searchbarfunction();
+ console.log(a)
+// search bar worked finished
